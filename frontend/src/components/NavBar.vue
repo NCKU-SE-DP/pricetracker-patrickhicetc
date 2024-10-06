d767fa45d2c333b7e82d86b2d9cd292b50d755df
patrickhicetc <spp0515@gmail.com>
2024-10-06 22:28:06 +0800

    feat: 在navbar新增螢幕縮小時的漢堡選單

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,7 +1,10 @@
 <template>
     <nav class="navbar">
-        <div class="title"> <RouterLink to="/overview">價格追蹤小幫手</RouterLink></div>
-        <ul class="options">
+        <div class="Title">
+            <div class="title"> <RouterLink to="/overview">價格追蹤小幫手</RouterLink></div>
+            <button class="hamburger" @click="isclickham">{{ (hamburger ? "X":"&#9776;") }}</button>
+        </div>
+        <ul class="options" v-if="hamburger||!isscreensmall">
             <li><RouterLink to="/overview">物價概覽</RouterLink></li>
             <li><RouterLink to="/trending">物價趨勢</RouterLink></li>
             <li><RouterLink to="/news">相關新聞</RouterLink></li>
@@ -16,7 +19,16 @@ import { useAuthStore } from '@/stores/auth';
 
 export default {
     name: 'NavBar',
+    data(){
+        return{
+            hamburger: false,
+            windowwidth: window.innerWidth
+        }
+    },
     computed: {
+        isscreensmall(){
+            return this.windowwidth < 768;
+        },
         isLoggedIn(){
             const userStore = useAuthStore();
             return userStore.isLoggedIn;
@@ -26,7 +38,21 @@ export default {
             return userStore.getUserName;
         }
     },
+    mounted() {
+        window.addEventListener('resize', this.handleResize);
+        this.handleResize();
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    },
     methods: {
+        handleResize(){
+            this.windowwidth = window.innerWidth;
+            this.hamburger = this.windowwidth >= 768;
+        },
+        isclickham(){
+            this.hamburger = !this.hamburger;
+        },
         logout(){
             const userStore = useAuthStore();
             userStore.logout();
@@ -60,7 +86,7 @@ export default {
 }
 
 .navbar li {
-    color: #575B5D;
+    color: #aeb3b6;
     margin: 0 .5em;
     font-size: 1.2em;
 }
@@ -74,5 +100,40 @@ export default {
     text-decoration: none;
     color: #575B5D;
 }
-
+.hamburger {
+    display: none;
+    font-size: 24px;
+    cursor: pointer;
+}
+.Title{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+@media (max-width: 768px) {
+    .navbar{
+        flex-direction: column;
+        align-items: flex-start;
+        width: 100%;
+        padding: 0;
+    }
+    .navbar li{
+        border-bottom: 1px solid #cabebe;
+    }
+    .options{
+        flex-direction: column;
+        width: 100%;
+        gap: 10px;
+        text-align: center;
+        padding: 5px;
+        background-color: #e6ddf1;
+    }
+    .hamburger {
+        display: block;
+    }
+    .Title{
+        width: 100%;
+        padding: 18px;
+    }
+}
 </style>

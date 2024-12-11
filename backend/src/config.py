3bda9acb03fd0e9941cb7fd32e7ddb97e3820ab1
patrickhicetc <spp0515@gmail.com>
2024-12-11 23:40:00 +0800

    feat: get openai key and anthropic key from .env

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,13 +1,21 @@
+import os
+from dotenv import load_dotenv
+
 from pydantic_settings import BaseSettings
 
-class GlobalSettings(BaseSettings):
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
-        env_prefix = "AUTH_"
-        extra = "ignore"
+dotenv_path = os.path.join(os.path.dirname(__file__), "./.env") 
+load_dotenv(dotenv_path)
 
-    SENTRY_DSN: str = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
-    TRACES_SAMPLE_RATE: float = 1.0
-    PROFILES_SAMPLE_RATE: float = 1.0
-    FASTAPI_PREFIX: str = "/api/v1"
+class Config:
+
+    class GlobalSettings(BaseSettings):
+        SENTRY_DSN: str = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
+        TRACES_SAMPLE_RATE: float = 1.0
+        PROFILES_SAMPLE_RATE: float = 1.0
+        FASTAPI_PREFIX: str = "/api/v1"
+
+    class OpenAI():
+        OPENAI_KEY = os.getenv("OPENAI_KEY", "")
+
+    class Anthropic():
+        ANTHROPIC_KEY = os.getenv("ANTHROPIC_KEY", "")

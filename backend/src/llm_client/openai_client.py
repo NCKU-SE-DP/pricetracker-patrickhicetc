09980fe63914e748b2019734b192d2ba5e9637b0
patrickhicetc <spp0515@gmail.com>
2024-12-19 19:32:09 +0800

    fix: fix openai client

--- backend/src/llm_client/openai_client.py
+++ backend/src/llm_client/openai_client.py
@@ -7,6 +7,6 @@ class OpenAIClient(LLMClientTemplate):
     def _initialize_client(self):
         self.client = ai.Client({"openai": {"api_key": self.api_key}})
         # self.model = "openai:gpt-4o"
-        self.model = "openai:gpt-3.5-turbo"
+        self.model = "openai:gpt-3.5-turbo" 
 
     
